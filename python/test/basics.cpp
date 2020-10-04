461c69b3e4f41dd3f1987699e3740260300dc773
Max1Truc <maxitruc.bonbec@gmail.com>
2020-10-04 16:34:51 +0200

    Fix python tests using removed default scripts

--- python/test/basics.cpp
+++ python/test/basics.cpp
@@ -8,8 +8,4 @@ QUIZ_CASE(python_basics) {
 }
 
 QUIZ_CASE(python_template) {
-  assert_script_execution_succeeds(Code::ScriptTemplate::Squares()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Mandelbrot()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Polynomial()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Parabola()->content());
 }
